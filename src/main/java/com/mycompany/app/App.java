f698198473a87849a8abc4aeb2c4bb7e268f1937
Wan Qing <wan-qing.tan@keysight.com>
2025-04-09 16:07:17 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkin is so boring!");
+        System.out.println("Modified from local project!");
     }
 
     public String getMessage() {
