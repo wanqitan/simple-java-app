cb2d539b0997ae7eff1941adb4aebaef51c45d59
Wan Qing <wan-qing.tan@keysight.com>
2025-04-10 14:15:40 +0800

    Next change for new pipeline project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified from local project!");
+        System.out.println("Some changes for pipeline");
     }
 
     public String getMessage() {
