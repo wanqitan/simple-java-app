7b6d30a3255ed6684cdb30509b4e91491a81efcb
Wan Qing <wan-qing.tan@keysight.com>
2025-04-10 14:54:51 +0800

    Testing out for pipeline

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Some changes for pipeline");
+        System.out.println("Some changes for pipeline!!");
     }
 
     public String getMessage() {
